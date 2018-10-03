bd1d346ae5119db70f498aa4eaee66cabe60d022
Silchenko <kyvinept@gmail.com>
2018-10-03 11:20:16 +0300

    add enterRegion

--- TestProject/ViewController/FourthViewController.swift
+++ TestProject/ViewController/FourthViewController.swift
@@ -20,6 +20,7 @@ class FourthViewController: UIViewController {
         self.title = NSLocalizedString("FourthViewController", comment: "")
         self.navigationController?.delegate = self
         setLocationManager()
+        setMonitoringEnterRegion()
     }
     
     private func setLocationManager() {
@@ -31,21 +32,72 @@ class FourthViewController: UIViewController {
         locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
     }
+    
+    private func setMonitoringEnterRegion() {
+        self.addRadiusCircle(location: CLLocation(latitude: 37.6879, longitude: -122.4702))
+        let region = CLCircularRegion(center: CLLocationCoordinate2D(latitude: 37.6879, longitude: -122.4702),
+                                      radius: 4000,
+                                  identifier: "Circle")
+        region.notifyOnEntry = true
+        region.notifyOnExit = false
+        locationManager.startMonitoring(for: region)
+    }
+    
+    private func addRadiusCircle(location: CLLocation){
+        let circle = MKCircle(center: location.coordinate, radius: 4000)
+        self.mapView.add(circle)
+    }
+}
+
+extension FourthViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKCircle {
+            let circle = MKCircleRenderer(overlay: overlay)
+            circle.strokeColor = UIColor.red
+            circle.fillColor = UIColor(red: 255,
+                                     green: 0,
+                                      blue: 0,
+                                     alpha: 0.3)
+            circle.lineWidth = 1
+            return circle
+        } else {
+            return MKPolylineRenderer()
+        }
+    }
 }
 
 extension FourthViewController: CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let location = locations.first?.coordinate {
-            let region = MKCoordinateRegion(center: location, span: MKCoordinateSpanMake(0.005, 0.005))
+            let region = MKCoordinateRegion(center: location, span: MKCoordinateSpanMake(0.2, 0.2))
             mapView.setRegion(region, animated: true)
         }
     }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-        let alert = UIAlertController(title: "Error", message: "Check your internet connection or allow this app tracking your location.", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Try again", style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
+        let alert = UIAlertController(title: "Error",
+                                    message: "Check your internet connection or allow this app tracking your location.",
+                             preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Try again",
+                                      style: .default,
+                                    handler: nil))
+        self.present(alert,
+                     animated: true,
+                   completion: nil)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        let alert = UIAlertController(title: "Enter",
+                                    message: "You enter in circle",
+                             preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok",
+                                      style: .default,
+                                    handler: nil))
+        self.present(alert,
+                     animated: true,
+                   completion: nil)
     }
 }
 
