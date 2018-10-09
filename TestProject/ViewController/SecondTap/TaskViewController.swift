bd4cbd0c87c369310d00b3ce05809dbfd1350ec5
Silchenko <kyvinept@gmail.com>
2018-10-09 15:48:50 +0300

    taskviewcontroller without add tasks

--- TestProject/ViewController/SecondTap/TaskViewController.swift
+++ TestProject/ViewController/SecondTap/TaskViewController.swift
@@ -39,23 +39,10 @@ class TaskViewController: BaseViewController {
         data.loadData { (modelTasks) in
             self.tasks = modelTasks
             self.tasks.reverse()
-            self.createTasks()
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }
-    }
-    
-    private func createTasks() {
-        let group = DispatchGroup()
-        group.enter()
-        for i in 0...100000 {
-            let task = self.createNewTask(withName: String(i) + " task name")
-            self.tasks.insert(task, at: 0)
-            self.data.addTask(withModel: task)
-        }
-        group.leave()
-        
-        group.notify(queue: DispatchQueue.main, work: DispatchWorkItem(block: {
-            self.tableView.reloadData()
-        }))
     }
     
     @objc func addButtonTapped() {
